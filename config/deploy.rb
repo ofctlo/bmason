81a4a37ed597f0e9086524551ede2f1d51e9c6c8
Brian Mason <brian.dale.mason@gmail.com>
2014-08-07 15:58:51 -0700

    Give Capistrano login shell
    Maybe then the environment variables will be properly set?

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application,   'bmason'
 set :deploy_to,     "/home/#{user}/apps/#{application}"
 set :deploy_via,    :remote_cache
 set :use_sudo,      false
-set :default_shell, '/bin/bash'
+set :default_shell, '/bin/bash -l'
 
 set :scm,        'git'
 set :repository, "git@github.com:ofctlo/#{application}.git"
