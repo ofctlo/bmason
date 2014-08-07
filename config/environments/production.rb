5d3b9b78b6c0f446302c85ce799137283ee295a5
Brian Mason <brian.dale.mason@gmail.com>
2014-08-07 16:22:32 -0700

    Add some ActionMailer config
    Be sure not to open source this!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,17 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'bmason.io',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    user_name:            'brian.dale.mason',
+    password:             'gategateparagateparasangatebodhisvaha',
+  }
+
+  config.action_mailer.default_url_options = { host: 'www.bmason.io' }
 end
