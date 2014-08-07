491b75fac92ba211460faa82684b0004653b09a0
Brian Mason <brian.dale.mason@gmail.com>
2014-08-07 15:16:25 -0700

    Fix params in mailer
    There was mismatched arity

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   default to: 'Brian Mason <brian.dale.mason+bmasonio@gmail.com>'
   default from: 'contact@bmason.io'
 
-  def contact(from, message)
+  def contact(name, from, message)
     mail to: RECIPIENT,
          from: SENDER,
          subject: "Email from: #{name} <#{from}>",
